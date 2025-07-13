b825938c1bd628a63ea10caae26acdddc634fd77
Babji Sheik <sheikbabjims@gmail.com>
2025-07-13 02:45:15 -0500

    final code

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/my-portfolio/', // Example: '/my-portfolio/'
+  base: './', // Example: '/my-portfolio/'
   assetsInclude: ['**/*.docx', '**/*.pdf'],
   plugins: [react()],
 })
